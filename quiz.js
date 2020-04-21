cd79a3f101703923118f7cf97873b0c4ed770930
gabdir <a.gabdrahmanov@innopolis.ru>
2020-04-21 15:56:14 +0300

    questions

--- quiz.js
+++ quiz.js
@@ -1,10 +1,23 @@
+const fs = require('fs')
+const path = require('path')
 
+const readQuiz = fs.readFileSync(path.join(__dirname, 'questions.json'))
+const jsonContent = JSON.parse(readQuiz)
+const title = jsonContent["title"]
 
-function sendQuestion(id) {
+const questions = jsonContent["questions"]
 
+function between(min, max){
+    return Math.floor(
+        Math.random() * (max - min + 1) + min
+    )
 }
 
-export function quiz() {
-    
-}
+module.exports = { 
+    sendQuestion() {
+    const rand = between(0, 10)
+    const question = questions[rand]
 
+    return question
+    }
+}

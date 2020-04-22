2e581ece519fdd29b5b80bfe5c11cc94f063463d
gabdir <a.gabdrahmanov@innopolis.ru>
2020-04-22 12:14:15 +0300

    quiz

--- data.js
+++ data.js
@@ -2,15 +2,23 @@ const quiz = require('./quiz')
 
 const questions = quiz.questions
 const data = []
+const prepared_questions = []
 
 let i; 
 for (i = 0; i < questions.length; i++){
-    data.push({title: `${questions[i].text}`, buttons: [[{text: 'Reply', callback_data: 'reply'}]], right_answer: `${questions[i].answers[questions[i].correct_answer]}`})
+    const title = `${questions[i].text}`
+    data.push({title: title, buttons: [{text: 'Reply', callback_data: 'reply'}], right_answer: `${questions[i].answers[questions[i].correct_answer]}`, prepared: `${[questions[i].answers]}`})
+    prepared_questions.push((questions[i].text,questions[i].answers))
+}
+
+for (i = 0; i < data.length; i ++) {
+    data[i].prepared = data[i].prepared.split(',')
 }
 
 module.exports = {
     data, 
     getRandomQuestion() {
-        return data[Math.floor(Math.random()*data.length)]
+        const rand = data[Math.floor(Math.random()*data.length)]
+        return rand 
     }
 }
--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const Telegraf = require('telegraf')
 const Markup = require('telegraf/markup')
 const Extra = require('telegraf/extra')
 
+const TelegrafInlineMenu = require('telegraf-inline-menu')
+const menu = new TelegrafInlineMenu(ctx => `Hey ${ctx.from.first_name}!`)
+menu.setCommand('start')
+ 
+menu.simpleButton('I am excited!', 'a', {
+  doFunc: ctx => ctx.reply('As am I!')
+})
+
 const quiz = require('./quiz')
 const prepared_data = require('./data')
 
@@ -9,34 +17,44 @@ const SocksAgent = require('socks5-https-client/lib/Agent');
 const socksAgent = new SocksAgent({
   socksHost: '127.0.0.1',
   socksPort: '9150',
-//   socksUsername: config.proxy.login,
-//   socksPassword: config.proxy.psswd,
 });
 
-function replyQuestion(msg) {
-    const arr = prepared_data.getRandomQuestion()
-    const text = arr.title
-    const options = {
-        reply_markup: JSON.stringify({inline_keyboard: arr.buttons, parse_mode: 'Markdown'})
-    }
-    message = msg.message
-    chat = message.hasOwnProperty('chat') ?   message.chat.id : message.from.id
-    msg.telegram.sendMessage(chat, text, options)
-}
 
 const bot = new Telegraf(process.env.BOT_TOKEN, {
     telegram: { agent: socksAgent }
 });
 
+bot.start((ctx) => {
+    return ctx.reply(`Добро пожаловать! 
+Если ты хочешь узнать о командах - набери /help
+Если же ты хочешь начать квиз - набери /quiz
+    `);
+
+
+});
+
 bot.hears(/\/help/, (ctx) => {
     ctx.reply('Here are available commands:',
-    Extra.HTML())
-    ctx.reply('/start - To start messaging', Extra.HTML())
-    ctx.reply('/help - To see available commands', Extra.HTML())
-    ctx.reply('/quiz - To start the quiz', Extra.HTML())
+    Extra.HTML()).then(() => {
+        ctx.reply('/start - To start messaging', Extra.HTML())
+        ctx.reply('/help - To see available commands', Extra.HTML())
+        ctx.reply('/quiz - To start the quiz', Extra.HTML())
+    })
 })
 
-bot.hears(/\/start/, (ctx) => {
+bot.hears(/\/quiz/, (ctx) => {
+    ctx.reply('Starting the quiz.').then(() => {
+        let arr = prepared_data.getRandomQuestion()
+        ctx.reply(arr.title, Extra.HTML()
+                            .markup(Markup.inlineKeyboard([Markup.callbackButton(arr.buttons[0].text, arr.buttons[0].callback_data)])))
+        bot.action('reply', (ctx) => {
+            ctx.replyWithQuiz(arr.title, arr.prepared,  { correct_option_id: arr.right_answer })
+        })
+    })  
+})
+
+
+bot.on('message', (ctx) => {
     ctx.reply(`Hello, ${ctx.from.first_name}. How are you?`,
     Extra.HTML()
     .markup(Markup.inlineKeyboard([
@@ -46,24 +64,16 @@ bot.hears(/\/start/, (ctx) => {
 })
 
 bot.action('not bad', (ctx) => {
-    ctx.editMessageText('<i>Have a nice day 😊</i>',
+    ctx.editMessageText(`<i>Have a nice day 😊</i>
+Type /quiz if you want to start a quiz.`,
         Extra.HTML())
 })
 bot.action('all right', (ctx) => {
-    ctx.editMessageText('<i>May happiness be with you 🙏</i>',
+    ctx.editMessageText('<i>May happiness be with you 🙏</i> Type /quiz if you want to start a quiz.',
         Extra.HTML())
 })
 
 
-bot.hears(/\/quiz/, (ctx) => {
-    ctx.reply('Starting quiz...').then(() => {
-        replyQuestion(ctx)
-    })  
-})
 
-bot.on('reply', (ctx) => {
-    result = []
-    ctx.reply('Nice')
-})
 
 bot.launch()
